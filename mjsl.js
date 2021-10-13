32f5250988345f4b9c560d1c02475258906fb30f
Jeffreymaniac <78237937+Jeffreymaniac@users.noreply.github.com>
2021-10-13 15:20:12 -0400

    Update mjsl.js

--- mjsl.js
+++ mjsl.js
@@ -3,7 +3,14 @@ document.getElementById(id).innerHTML = code;
 }
 function mjslObject(id, object, input) {
 if(object == "box") {
-document.getElementById(id).innerHTML = "<div style="border: black 2px solid;">" + input + "</div>";
+document.getElementById(id).innerHTML = "<div id="mjsl-box">" + input + "</div>";
 }
   
 }
+
+var jsstyles = document.getElementById("mjsl-styles");
+jsstyles.innerHTML = "
+#mjsl-box {
+border: 2px solid black;
+}
+"
