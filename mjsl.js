ff7bd3b9cd9b5cbe363025154fcbd0b21c914a53
Jeffreymaniac <78237937+Jeffreymaniac@users.noreply.github.com>
2021-10-13 13:30:39 -0400

    Update mjsl.js

--- mjsl.js
+++ mjsl.js
@@ -1,6 +1,9 @@
 function changeHTML(id, code) {
 document.getElementById(id).innerHTML = code;
 }
-function mjslElement(element, input) {
-
+function mjslObject(id, object, input) {
+if(object == "box") {
+document.getElementById(id).innerHTML = "<div style="border: black 2px solid;">" + input + "</div>";
+}
+  
 }
