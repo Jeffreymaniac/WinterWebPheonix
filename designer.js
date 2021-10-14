a6a44513692efaef3a998c1fab6dcdaf0f30bbf5
Jeffreymaniac <78237937+Jeffreymaniac@users.noreply.github.com>
2021-10-14 16:03:04 -0400

    Create designer.js

---
+++ designer.js
@@ -0,0 +1,8 @@
+function djsStyle(id, style) {
+if(style == "box") {
+document.getElementById(id).style.border = "solid black";
+}
+if(style == "bold") {
+document.getElementById(id).style.fontWeight="bold";
+}  
+}
