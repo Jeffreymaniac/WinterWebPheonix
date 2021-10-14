16b5be1778977b6e9560ec976050a5ebf1fbface
Jeffreymaniac <78237937+Jeffreymaniac@users.noreply.github.com>
2021-10-14 15:39:41 -0400

    Update mjsl.js

--- mjsl.js
+++ mjsl.js
@@ -1,11 +1,13 @@
 function changeHTML(id, code) {
 document.getElementById(id).innerHTML = code;
 }
-function mjslObject(id, object) {
-if(object == "box") {
+function mjslStyle(id, style) {
+if(style == "box") {
 document.getElementById(id).style.border = "solid black";
 }
-  
+if(style == "bold") {
+document.getElementById(id).style.fontWeight="bold";
+}  
 }
 
 
