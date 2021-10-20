985bb5fc4cb302cd94e818e56a07336d00447efe
Jeffreymaniac <78237937+Jeffreymaniac@users.noreply.github.com>
2021-10-20 15:45:15 -0400

    Update designer.js

--- designer.js
+++ designer.js
@@ -6,3 +6,9 @@ if(style == "bold") {
 document.getElementById(id).style.fontWeight="bold";
 }  
 }
+function djsIcon(id, icon) {
+var i = document.getElementById(id);
+if(icon == "android-icon-black") {
+  i.innerHTML = "<img src='https://cdn-icons-png.flaticon.com/512/38/38002.png' />";
+  }
+}
