d363f776c517aef3d925b1a4b012fa303f470f4c
Jeffreymaniac <78237937+Jeffreymaniac@users.noreply.github.com>
2021-11-12 15:03:26 -0500

    Update designer.js

--- designer.js
+++ designer.js
@@ -1,11 +1,23 @@
+var canvas = document.getElementById("djs-canvas");
+var ctx = canvas.getContext("2d");
+
+
+function makeCircle(x, y, size) {
+ctx.beginPath();
+ctx.arc(x, y, size, 0, 2 * Math.PI);
+ctx.stroke();
+}
+
 function djsStyle(id, style) {
 if(style == "box") {
 document.getElementById(id).style.border = "solid black";
 }
 if(style == "bold") {
 document.getElementById(id).style.fontWeight="bold";
-}  
 }
+  
+}
+
 function djsIcon(id, icon) {
 var i = document.getElementById(id);
 if(icon == "android-icon-black") {
